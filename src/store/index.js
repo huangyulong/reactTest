1b986b068fe8404ca460d523d87da30cff455c04
GTR <need_for_speed_9@163.com>
2018-06-05 17:58:00 +0800

    changeRedux

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import { createStore } from 'redux'
-// import changeAge from '../reducer/index'
+import { createStore, applyMiddleware } from 'redux'
 import lightReducer from '../reducer/index'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 
-// export default state = {
-//     name:'',
-//     age:10
+const logger = createLogger();
+export default store = createStore(
+    lightReducer,
+    applyMiddleWare(thunkMiddleware , logger )
+)
+
+// const createStoreWithMiddleware = applyMiddleware(
+//     thunkMiddleware,
+//     loggerMiddleware
+//   )(createStore);
+
+// export default function lightStore(initstate){
+//     return createStoreWithMiddleware(lightReducer,initstate)
 // }
-
-
-export default function lightStore(initstate){
-    return createStore(lightReducer,initstate)
-}
 
 
